7e559409b1fa1d532e298c24382cfe8940396cc3
kuttappi <iamkuttappiii@gmail.com>
2024-10-25 20:29:14 +0530

    responsive and cursor png is updated

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ function opennav(){
     document.getElementById("navbar").style.display="flex"
     document.getElementById("this").style.display="none"
 }
+
+
+
+
+
+// cursor 
+
+
+
+
+// const cursor = document.querySelector(".cursor");
+// var timeout;
+
+
+// document.addEventListener('mousemove', (e) =>{
+//     let x = e.pageX;
+//     let y = e.pageY;
+
+//    cursor.style.top = y + "px";
+//    cursor.style.left = x + "px";
+//    cursor.style.display = "block";
+
+//    function mousestopped(){
+//     cursor.style.display = "none"
+// }
+
+// clearTimeout(timeout);
+// timeout = setTimeout(mousestopped,1000)
+
+// });
+
+// // cursor style on mouse
+
+// document.addEventListener("mouseout",() =>{
+//     cursor.style.display = "none";
+// });
+
