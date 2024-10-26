e9b68e51360e5488a19037102c70415ad5c20d80
kuttappi <iamkuttappiii@gmail.com>
2024-10-26 23:15:03 +0530

    project section added

--- script.js
+++ script.js
@@ -8,6 +8,41 @@ function opennav(){
 
 
 
+// link opening 
+
+function openlink(a){
+    if( a == 1){
+        window.location.href= 'https://javaadde.github.io/GreenApple.in/';
+    }
+    else if(a == 2){
+        window.location.href= '#home';
+    }
+    else if(a == 3){
+        window.location.href= 'https://javaadde.github.io/Calculator/';
+    } 
+    else if(a == 4){
+        window.location.href= 'https://javaadde.github.io/javad.io/';
+    }
+    else if(a == 5){
+        window.location.href= 'https://javaadde.github.io/greenapplestore/';
+    }
+}
+
+
+// repo button open
+
+
+    function Openrep(){
+
+    document.getElementById("inside").style.display="flex";
+    document.getElementById("bottum-slider").style.display="none";
+
+}
+
+
+
+
+
 
 
 // cursor 
@@ -15,30 +50,30 @@ function opennav(){
 
 
 
-// const cursor = document.querySelector(".cursor");
-// var timeout;
+const cursor = document.querySelector(".cursor");
+var timeout;
 
 
-// document.addEventListener('mousemove', (e) =>{
-//     let x = e.pageX;
-//     let y = e.pageY;
+document.addEventListener('mousemove', (e) =>{
+    let x = e.pageX;
+    let y = e.pageY;
 
-//    cursor.style.top = y + "px";
-//    cursor.style.left = x + "px";
-//    cursor.style.display = "block";
+   cursor.style.top = y + "px";
+   cursor.style.left = x + "px";
+   cursor.style.display = "block";
 
-//    function mousestopped(){
-//     cursor.style.display = "none"
-// }
+   function mousestopped(){
+    cursor.style.display = "none"
+}
 
-// clearTimeout(timeout);
-// timeout = setTimeout(mousestopped,1000)
+clearTimeout(timeout);
+timeout = setTimeout(mousestopped,1000)
 
-// });
+});
 
-// // cursor style on mouse
+// cursor style on mouse
 
-// document.addEventListener("mouseout",() =>{
-//     cursor.style.display = "none";
-// });
+document.addEventListener("mouseout",() =>{
+    cursor.style.display = "none";
+});
 
