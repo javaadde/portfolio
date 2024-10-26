fbc4f337d6cb9bc3e6e798cf9b00db42c0956e97
kuttappi <iamkuttappiii@gmail.com>
2024-10-26 08:17:34 +0530

    responsive navabar erro fixed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function opennav(){
     document.getElementById("navbar").style.display="flex"
     document.getElementById("this").style.display="none"
+    document.getElementById("jd-mb").style.marginTop="80px"
 }
 
 
