a12409c763585c084dfaf71b5055c5addf286871
kuttappi <iamkuttappiii@gmail.com>
2024-10-27 19:00:32 +0530

    ok

--- script.js
+++ script.js
@@ -10,22 +10,8 @@ function opennav(){
 
 // link opening 
 
-function openlink(a){
-    if( a == 1){
-        window.location.href= 'https://javaadde.github.io/GreenApple.in/';
-    }
-    else if(a == 2){
-        window.location.href= '#home';
-    }
-    else if(a == 3){
-        window.location.href= 'https://javaadde.github.io/Calculator/';
-    } 
-    else if(a == 4){
-        window.location.href= 'https://javaadde.github.io/javad.io/';
-    }
-    else if(a == 5){
-        window.location.href= 'https://javaadde.github.io/greenapplestore/';
-    }
+function openlink(){
+   window.location.href="./none-public/projects.html"
 }
 
 
