b33021b898e8c3416aff17431289d9f43aaddc1a
kuttappi <iamkuttappiii@gmail.com>
2024-10-30 03:19:10 +0530

    updating well a new theme of website having changeble theme like dark and light

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
 
-function opennav(){
-    document.getElementById("navbar").style.display="flex"
-    document.getElementById("this").style.display="none"
-    document.getElementById("jd-mb").style.marginTop="80px"
-}
+// function opennav(){
+//     document.getElementById("navbar").style.display="flex"
+//     document.getElementById("this").style.display="none"
+//     document.getElementById("jd-mb").style.marginTop="80px"
+// }
 
 
 
 // link opening 
 
 function openlink(){
-   window.location.href="./none-public/projects.html"
+   window.location.href="./none-public/projects.html";
 }
 
 
@@ -61,3 +61,65 @@ document.addEventListener("mouseout",() =>{
 
 
 
+// theme
+
+
+function theme(){
+
+     
+
+    const root = document.documentElement;
+    const currentColor = getComputedStyle(root).getPropertyValue('--last').trim();
+
+  
+    // Toggle between two colors
+  
+    if (currentColor === 'rgb(63, 250, 110') {
+      root.style.setProperty('--last', '#000');
+      root.style.setProperty('--ooo','#fff');
+      root.style.setProperty('--body','#717b7a');
+      root.style.setProperty('--back','#b9dfda');
+      
+    } else {
+      root.style.setProperty('--last', 'rgb(63, 250, 110'); 
+      root.style.setProperty('--ooo','#000');
+      root.style.setProperty('--body','#131212');
+      root.style.setProperty('--back','#3e3e44');
+    }
+  
+   
+
+   
+      
+
+}
+
+///skill and tool  shifting
+
+
+  function toolchange(){
+    document.getElementById("span2").style.color="var(--ooo)"
+    document.getElementById("span2").style.backgroundColor="var(--last)"
+
+    document.getElementById("skills").style.display="none"
+    document.getElementById("tools").style.display="flex"
+
+
+    document.getElementById("span1").style.color="var(--last)"
+    document.getElementById("span1").style.backgroundColor="var(--ooo)"
+
+  }
+
+
+  function skillchange(){
+    document.getElementById("span1").style.color="var(--ooo)"
+    document.getElementById("span1").style.backgroundColor="var(--last)"
+
+    document.getElementById("skills").style.display="flex"
+    document.getElementById("tools").style.display="none"
+
+
+    document.getElementById("span2").style.color="var(--last)"
+    document.getElementById("span2").style.backgroundColor="var(--ooo)"
+
+  }
